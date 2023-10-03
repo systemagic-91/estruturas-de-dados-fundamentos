db95ded2774a52486d0f435ea5d994a6d4bad225
systemagic-91 <rayanepaiiva@live.com>
2023-10-03 13:17:24 -0300

    adicionados metodos de remoção e busca

--- src/main/java/org/db/cop/arvores/ArvoreBinariaBusca.java
+++ src/main/java/org/db/cop/arvores/ArvoreBinariaBusca.java
@@ -27,6 +27,67 @@ public class ArvoreBinariaBusca<T extends Comparable<T>> {
 
     return raiz;
   }
+  public boolean buscar(T dado) {
+    return buscarRec(raiz, dado);
+  }
+
+  private boolean buscarRec(NoArvore<T> raiz, T dado) {
+    if (raiz == null) {
+      return false;
+    }
+
+    int comparacao = dado.compareTo(raiz.dado);
+
+    if (comparacao == 0) {
+      return true;
+    } else if (comparacao < 0) {
+      return buscarRec(raiz.esquerda, dado);
+    } else {
+      return buscarRec(raiz.direita, dado);
+    }
+  }
+
+  public void remover(T dado) {
+    raiz = removerRec(raiz, dado);
+  }
+
+  private NoArvore<T> removerRec(NoArvore<T> raiz, T dado) {
+    if (raiz == null) {
+      return raiz;
+    }
+
+    int comparacao = dado.compareTo(raiz.dado);
+
+    if (comparacao < 0) {
+      raiz.esquerda = removerRec(raiz.esquerda, dado);
+    } else if (comparacao > 0) {
+      raiz.direita = removerRec(raiz.direita, dado);
+    } else {
+      // Nó com um ou nenhum filho
+      if (raiz.esquerda == null) {
+        return raiz.direita;
+      } else if (raiz.direita == null) {
+        return raiz.esquerda;
+      }
+
+      // Nó com dois filhos, encontrar o sucessor inordem (menor à direita)
+      raiz.dado = minValue(raiz.direita);
+
+      // Remover o sucessor inordem
+      raiz.direita = removerRec(raiz.direita, raiz.dado);
+    }
+
+    return raiz;
+  }
+
+  private T minValue(NoArvore<T> raiz) {
+    T minValue = raiz.dado;
+    while (raiz.esquerda != null) {
+      minValue = raiz.esquerda.dado;
+      raiz = raiz.esquerda;
+    }
+    return minValue;
+  }
 
   public void percorrerEmOrdem() {
     percorrerEmOrdem(raiz);
@@ -39,6 +100,8 @@ public class ArvoreBinariaBusca<T extends Comparable<T>> {
       percorrerEmOrdem(raiz.direita);
     }
   }
-
 }
+
+
+
 
